cbfaf1484394d5e80b7157154491e409c13eadd0
David Thorpe <davidthorpe878@gmail.com>
2019-03-23 19:55:43 +0000

    new gatsby files

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,8 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`, `plants`]} />
+    <h1>Welcome to my Plant site.</h1>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
